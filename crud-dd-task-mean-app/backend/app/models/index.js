45f7f7544b595f98253b9bbe318f697b059d0c33
saishankarreddy <saishankareddykarri@gmail.com>
2026-02-24 16:18:23 +0530

    Fix MongoDB Docker networking, Nginx proxy, and frontend build

--- crud-dd-task-mean-app/backend/app/models/index.js
+++ crud-dd-task-mean-app/backend/app/models/index.js
@@ -1,11 +1,15 @@
-const dbConfig = require("../config/db.config.js");
-
 const mongoose = require("mongoose");
+
+
+  process.env.MONGO_URI || "mongodb://localhost:27017/meanapp";
+
 mongoose.Promise = global.Promise;
 
 const db = {};
 db.mongoose = mongoose;
-db.url = dbConfig.url;
+db.url = MONGO_URI;
+
+// Import models
 db.tutorials = require("./tutorial.model.js")(mongoose);
 
 module.exports = db;
